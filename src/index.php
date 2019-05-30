e08854741b5148eef6901114f8df646b46c63d54
Ben Schenerman <ben.schenerman@gmail.com>
2019-05-30 18:49:10 -0400

    ✨ Add temp index page

---
+++ src/index.php
@@ -0,0 +1,13 @@
+
+<?php
+    require_once('includes/_header.php');
+    // include('includes/_db.php');
+?>
+
+<h1>Henlo Worl</h1>
+<a href="search.php">Search</a>
+<a href="recipe.php">Recipe</a>
+
+<?php    
+    require_once('includes/_footer.php')
+?>
