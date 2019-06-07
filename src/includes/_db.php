3c88c181b92bbbfd650d3cb37364ff58cb3f493e
Ben Schenerman <ben.schenerman@gmail.com>
2019-06-06 20:11:30 -0400

    🔨 Rm comment from db open call

--- src/includes/_db.php
+++ src/includes/_db.php
@@ -14,46 +14,4 @@
 
     mysqli_query($connection, "SET NAMES 'utf8'");
 
-
-    //------------
-    // Get recipe
-    // $queryRecipe = "SELECT * FROM recipe WHERE id LIKE '0da65b64c460b6887dc3109e77f1e481'";
-    // $resultRecipe = mysqli_query($connection, $queryRecipe);
-    // if (!$resultRecipe) {
-    //     die("Database query failed.");
-    // }
-    // while($row = $resultRecipe->fetch_assoc()) {
-        
-    //     // get associated kitchen tool
-    //     echo $row['kitchenToolid'];
-    //     echo "<hr>";
-
-    //     $queryKitchenTool = "SELECT * FROM kitchenTool WHERE id={$row['kitchenToolid']}";
-    //     $resultKitchenTool = mysqli_query($connection, $queryKitchenTool);
-    //     if (!$resultKitchenTool) {
-    //         die("Database query failed.");
-    //     }
-    //     while($kitchenRow = $resultKitchenTool->fetch_assoc()) {
-    //         var_dump($kitchenRow);
-    //         echo "<hr>";
-    //     }
-
-    //     // get associated howTo
-    //     echo $row['howToid'];
-    //     echo "<hr>";
-
-    //     $queryHowTo = "SELECT * FROM kitchenTool WHERE id={$row['howToid']}";
-    //     $resultHowTo = mysqli_query($connection, $queryHowTo);
-    //     if (!$resultHowTo) {
-    //         die("Database query failed.");
-    //     }
-    //     while($howRow = $resultHowTo->fetch_assoc()) {
-    //         var_dump($howRow);
-    //         echo "<hr>";
-    //     }
-        
-    // }
-
-    //------------
-
 ?>
