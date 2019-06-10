9563353d7f26e3c5154c6d0a53a20478163cd601
Ben Schenerman <ben.schenerman@gmail.com>
2019-06-10 01:17:10 -0400

    ✨ Redirect index to search

--- src/index.php
+++ src/index.php
@@ -1,13 +1,4 @@
-
-<?php
-    require_once('includes/_header.php');
-    // include('includes/_db.php');
-?>
-
-<h1>Henlo Worl</h1>
-<a href="search.php">Search</a>
-<a href="recipe.php">Recipe</a>
-
 <?php    
-    require_once('includes/_footer.php')
+    header('Location: ' . 'search.php');
+    die();
 ?>
