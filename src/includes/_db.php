313863a2f29c16b274a7ca2ee02365943949cf7d
Ben Schenerman <ben.schenerman@gmail.com>
2019-06-10 01:18:00 -0400

    ✨ Add remote db connection

--- src/includes/_db.php
+++ src/includes/_db.php
@@ -1,17 +1,27 @@
 <?php
+  $checkHost = $_SERVER['HTTP_HOST'];
+  if($checkHost == 'localhost:8080'){
     $dbhost = "localhost";
     $dbuser = "root";
     $dbpass = "";
     $dbname = "idm232";
-    $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+  }else{
+    $dbhost = "localhost";
+    $dbuser = "***REMOVED***";
+    $dbpass = "***REMOVED***";
+    $dbname = "***REMOVED***";
+  }
+  
+  
+  $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
-    if (mysqli_connect_errno()) {
-        die("Database connection failed: " .
-          mysqli_connect_error() .
-          " (" . mysqli_connect_errno() . ")"
-        );
-    }
+  if (mysqli_connect_errno()) {
+      die("Database connection failed: " .
+        mysqli_connect_error() .
+        " (" . mysqli_connect_errno() . ")"
+      );
+  }
 
-    mysqli_query($connection, "SET NAMES 'utf8'");
+  mysqli_query($connection, "SET NAMES 'utf8'");
 
 ?>
